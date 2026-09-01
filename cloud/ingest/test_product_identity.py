1ebbd7ef67c91c3cda3762f4937560de48943e90
Malik05255 <mmbbrr99@gmail.com>
2026-09-01 03:12:38 +0300

    test: lock promotional bonus-size identity behavior

--- cloud/ingest/test_product_identity.py
+++ cloud/ingest/test_product_identity.py
@@ -56,6 +56,20 @@ class ProductIdentityTests(unittest.TestCase):
         self.assertEqual(one.net_content_value, Decimal("1000.000"))
         self.assertEqual(one.net_content_unit, "ml")
 
+    def test_bonus_mass_is_summed_before_identity(self):
+        promo = derive_identity(product("12345670", "Nutella Hazelnut Spread 750 + 75 G", "Nutella"))
+        total = derive_identity(product("12345688", "Nutella Hazelnut Spread 825 G", "Nutella"))
+        self.assertEqual(promo.net_content_value, Decimal("825.000"))
+        self.assertEqual(promo.net_content_unit, "g")
+        self.assertEqual(promo.identity_key, total.identity_key)
+
+    def test_bonus_volume_with_units_on_both_terms_is_summed(self):
+        promo = derive_identity(product("12345670", "Juice Original 1 L + 250 ml", "Example"))
+        total = derive_identity(product("12345688", "Juice Original 1250 ml", "Example"))
+        self.assertEqual(promo.net_content_value, Decimal("1250.000"))
+        self.assertEqual(promo.net_content_unit, "ml")
+        self.assertEqual(promo.identity_key, total.identity_key)
+
 
 if __name__ == "__main__":
     unittest.main()
